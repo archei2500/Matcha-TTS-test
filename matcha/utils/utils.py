3d3e2c2df93734e6498194e264bfc7cd41d8164f
Irinus <irinamayborodina@gmail.com>
2025-08-16 18:55:48 +0300

    fix outdated method in utils

--- matcha/utils/utils.py
+++ matcha/utils/utils.py
@@ -136,7 +136,7 @@ def intersperse(lst, item):
 
 
 def save_figure_to_numpy(fig):
-    data = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep="")
+    data = np.fromstring(fig.canvas..buffer_rgba(), dtype=np.uint8, sep="")
     data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
     return data
 
